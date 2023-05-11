35287f1c355c2d337ad7994439fab228bf4d8ddc
Kyuwoong Lee <kwooler@gmail.com>
2023-05-11 14:41:31 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def main():
+    mylist = [5, 10, 15, 25, 20, 55, 40]
+    #########################################
+    # Code your program here
+    #########################################
+
+    #########################################
+    # Do not delete the reutrn statement
+    return result
+
+
+if __name__ == '__main__':
+    main()
